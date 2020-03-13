58004b5e00a3cf47789d578a6d0be05722ffddf4
Roarpalm <307687673@qq.com>
2020-03-13 11:02:34 +0800

    Update 1.3

--- B站视频播放量.py
+++ B站视频播放量.py
@@ -3,7 +3,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-import time, os, sys, you_get, re
+from concurrent.futures import ThreadPoolExecutor
+from time import sleep
+import re
 
 def browser(up_url):
     chrome_opt = Options()
@@ -15,37 +17,50 @@ def browser(up_url):
     driver = webdriver.Chrome(chrome_options=chrome_opt)
     driver.get(up_url)
 
+    titles = []
+    links = []
+    plays = []
+    dates = []
+
     try:
-        urls = []
         wait = WebDriverWait(driver, 10)
-        name = wait.until(EC.presence_of_element_located((By.NAME, 'h-name')))
         final_page = str(wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="submit-video-list"]/ul[3]/span[1]'))).text)
         num = int(re.search(r'\d+', final_page)[0])
         for _ in range(num):
-            elems = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="submit-video-list"]/ul[2]/li/a[1]')))
-            for i in elems:
-                url = i.get_attribute('href')
-                urls.append(url)
+            old_titles = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="submit-video-list"]/ul[2]/li/a[2]')))
+            for i in old_titles:
+                title = i.get_attribute('title')
+                print(title)
+                link = i.get_attribute('href')
+                titles.append(title)
+                links.append(link)
+                
+            old_plays = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="submit-video-list"]/ul[2]/li/div/span[1]')))
+            for i in old_plays:
+                play = i.text
+                plays.append(play)
 
+            old_dates = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="submit-video-list"]/ul[2]/li/div/span[2]')))
+            for i in old_dates:
+                date = i.text
+                dates.append(date)
             next_page = wait.until(EC.presence_of_element_located((By.LINK_TEXT, '下一页')))
             if next_page:
-                time.sleep(5)
+                sleep(5)
                 next_page.click()
                 driver.refresh()
                 print('next_page')
-        
-        path = os.path.abspath('.') + f'//{name}'
-        [download(path, url) for url in urls]
+
     except Exception as e:
         print(e)
 
-def download(path, url):
-    sys.argv = ['you-get', '-o', path, url]
-    you_get.main()
+    all_list = list(zip(titles, links, plays, dates))
+    with open('播放量.txt', 'w', encoding='utf-8') as f:
+        for title, link, play, date in all_list:
+            f.write(f'标题：{title}\n链接：{link}\n播放量：{play}\n日期：{date}\n\n')
 
 if __name__ == "__main__":
-    # UP个人空间视频url
-    # 日食记 = 'https://space.bilibili.com/8960728/video'
+    # 我的B站视频：'https://space.bilibili.com/9174628/video'
     up = input('请输入UP数字ID：')
     up_url = f'https://space.bilibili.com/{up}/video'
     browser(up_url)
